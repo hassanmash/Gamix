d7f90abd7df90c1ebbec9f2bdaba6dca1849be80
hassanmash <mashmoomashraf@gmail.com>
2021-08-11 22:57:48 +0530

    New features

--- app/src/main/java/com/example/gamix/MainActivity.java
+++ app/src/main/java/com/example/gamix/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.gamix;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
